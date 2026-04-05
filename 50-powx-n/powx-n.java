26a1fdb4b9bbe6ac3a7908bb541aeff5ce9633bc
Shubham Nitin Chaudhari <shubhamchaudhari2007@gmail.com>
2026-04-05 23:26:11 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (27.53%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,15 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N=n;
+        if(n<0){
+        return 1.0/pow(x,-N);
+        }
+        return pow(x,N);
+    }
+    double pow(double x,long n){
+        if(n==0){return 1.0;}
+        if(n==1){return x;}
+        if(n%2==0){return pow(x*x,n/2);}
+        return x*pow(x,n-1);
+    }
+}
